9651dbd95cbf190d6d6420d747fad98adda6b8c7
Om-Shri <som667075@gmail.com>
2025-12-16 17:08:34 +0530

    added some new content

--- Prectice.py
+++ Prectice.py
@@ -1,11 +1,8 @@
 import requests
-from PIL import Image
-from io import BytesIO
 
+url = " "
+r = requests.get("URL")
 
-r = requests.get("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1txXfWlVt3L51daXBkH4smrrFTEu0ocAzfQ&s")
-
-i = Image.open(BytesIO(r.content))
-fp = open("image.jpg","wb")
-i.save(fp)
+fp = open("file.exe","wb")
+fp.write(r.content)
 fp.close()
