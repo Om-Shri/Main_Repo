0ae7e1e9e3f0fa974c423c1b273837df2c410841
Om-Shri <som667075@gmail.com>
2025-12-16 16:51:58 +0530

    create a new program

--- Prectice.py
+++ Prectice.py
@@ -1,3 +1,11 @@
+import requests
+from PIL import Image
+from io import BytesIO
 
 
+r = requests.get("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1txXfWlVt3L51daXBkH4smrrFTEu0ocAzfQ&s")
 
+i = Image.open(BytesIO(r.content))
+fp = open("image.jpg","wb")
+i.save(fp)
+fp.close()
